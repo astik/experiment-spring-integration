b0e7678b4aedc948f99ff2a6277c4b104c5c3e78
Romain Gonord <romain.gonord.opensource@neteyes.org>
2020-12-02 21:40:10 +0100

    add zip feature

--- src/main/java/fr/smile/poc/Main.java
+++ src/main/java/fr/smile/poc/Main.java
@@ -1,18 +1,25 @@
 package fr.smile.poc;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.core.GenericSelector;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.context.IntegrationContextUtils;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
-import org.springframework.integration.file.transformer.FileToStringTransformer;
+import org.springframework.integration.file.transformer.FileToByteArrayTransformer;
+import org.springframework.integration.handler.GenericHandler;
+import org.springframework.integration.zip.splitter.UnZipResultSplitter;
+import org.springframework.integration.zip.transformer.UnZipTransformer;
+import org.springframework.integration.zip.transformer.ZipResultType;
 import org.springframework.messaging.MessageHandler;
 
 import lombok.extern.slf4j.Slf4j;
@@ -45,28 +52,95 @@ public class Main {
     }
 
     @Bean
-    public GenericSelector<File> onlyCSV() {
-        return new GenericSelector<File>() {
-            @Override
-            public boolean accept(File source) {
-                return source.getName().endsWith(".csv");
-            }
-        };
+    public FileToByteArrayTransformer fileToByteArrayTransformer() {
+        return new FileToByteArrayTransformer();
     }
 
     @Bean
-    public FileToStringTransformer fileToStringTransformer() {
-        return new FileToStringTransformer();
+    public UnZipTransformer unZipTransformer() {
+        UnZipTransformer unZipTransformer = new UnZipTransformer();
+        unZipTransformer.setZipResultType(ZipResultType.BYTE_ARRAY);
+        return unZipTransformer;
     }
 
     @Bean
-    public IntegrationFlow fileMover() {
-        log.trace("fileMover");
+    public UnZipResultSplitter unZipSplitter() {
+        UnZipResultSplitter unZipResultSplitter = new UnZipResultSplitter();
+        return unZipResultSplitter;
+    }
+
+    @Bean
+    DirectChannel inputByteArrayChannel() {
+        log.trace("inputByteArrayChannel");
+        return new DirectChannel();
+    }
+
+    @Bean
+    DirectChannel csvChannel() {
+        log.trace("csvChannel");
+        return new DirectChannel();
+    }
+
+    @Bean
+    DirectChannel zipChannel() {
+        log.trace("zipChannel");
+        return new DirectChannel();
+    }
+
+    @Bean
+    public IntegrationFlow loadFile() {
+        log.trace("loadFile");
         return IntegrationFlows //
                 .from(sourceDirectory(), configurer -> configurer.poller(Pollers.fixedDelay(5000))) //
-                .filter(onlyCSV()) //
-                .transform(fileToStringTransformer()) //
-                .handle(targetDirectory()) //
+                .transform(fileToByteArrayTransformer()) //
+                .channel(inputByteArrayChannel()) //
                 .get();
+    }
+
+    @Bean
+    public IntegrationFlow dispatchByteArray() {
+        log.trace("dispatchByteArray");
+        return IntegrationFlows //
+                .from("inputByteArrayChannel") //
+                .route("T(org.apache.commons.io.FilenameUtils).getExtension(headers['file_name'])",
+                        routerConfigurer -> {
+                            routerConfigurer.resolutionRequired(false);
+                            routerConfigurer.channelMapping("csv", csvChannel());
+                            routerConfigurer.channelMapping("zip", zipChannel());
+                            routerConfigurer.defaultOutputChannel(IntegrationContextUtils.ERROR_CHANNEL_BEAN_NAME);
+                        }) //
+                .get();
+    }
+
+    @Bean
+    public IntegrationFlow processCsv() {
+        log.trace("processCsv");
+        return IntegrationFlows //
+                .from(csvChannel()) //
+                .channel("out") //
+                .get();
+    }
+
+    @Bean
+    public IntegrationFlow processZip() {
+        log.trace("processZip");
+        return IntegrationFlows //
+                .from(zipChannel()) //
+                .transform(unZipTransformer()) //
+                .split(unZipSplitter()) //
+                .filter("!headers['file_name'].startsWith('.')") //
+                .channel(inputByteArrayChannel()) //
+                .get();
+    }
+
+    @Bean
+    @Transformer(inputChannel = "out", outputChannel = IntegrationContextUtils.ERROR_CHANNEL_BEAN_NAME)
+    public GenericHandler<byte[]> logHandler() {
+        return (payload, headers) -> {
+            System.out.println("------------> logHandler");
+            System.out.println("---> headers " + headers);
+            System.out.println("---> payload " + new String(payload, StandardCharsets.UTF_8));
+            return payload;
+        };
     }
 }
